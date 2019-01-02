ce79d9aca432ff77018d6b51da4ef39bab7f5110
roubin <roubin@mydaydream.com>
2019-01-02 14:16:08 +0800

    添加传统的伸缩标题栏

--- app/src/main/java/com/example/apple/myapplication/MainActivity.java
+++ app/src/main/java/com/example/apple/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.apple.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,12 +11,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button_appbar).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.button_appbar:
+                startActivity(new Intent(this, AppBarActivity.class));
+                break;
+        }
     }
 }
