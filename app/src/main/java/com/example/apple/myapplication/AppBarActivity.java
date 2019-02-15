9c77a4f214f0ead7efebdf08dd55f26e7553ab4f
roubin <roubin@mydaydream.com>
2019-02-15 18:35:36 +0800

    在伸缩标题栏上实现颜色渐变

--- app/src/main/java/com/example/apple/myapplication/AppBarActivity.java
+++ app/src/main/java/com/example/apple/myapplication/AppBarActivity.java
@@ -1,13 +1,32 @@
 package com.example.apple.myapplication;
 
+import android.animation.ArgbEvaluator;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class AppBarActivity extends AppCompatActivity {
+    private int startColor = 0xFF008D96, endColor = 0xFFFFFFFF;
+    private LinearLayout item_title;
+    private ArgbEvaluator evaluator = new ArgbEvaluator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_bar);
+
+        AppBarLayout appBarLayout = findViewById(R.id.appbar_layout);
+        item_title = findViewById(R.id.item_title);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+                float percent = i / (float) -140;
+                item_title.setBackgroundColor((int) evaluator.evaluate(percent, startColor, endColor));
+            }
+        });
+
+
     }
 }
